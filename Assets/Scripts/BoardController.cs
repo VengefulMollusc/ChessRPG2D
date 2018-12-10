dcc32836fbf2ed3921549e63b6427dcc2cf09981
Chris Read <ChrisR@intergen.org.nz>
2018-12-10 13:22:42 +1300

    initial setup

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour
+{
+
+    private Board currentBoard;
+
+	// Use this for initialization
+	void Start ()
+	{
+        InitialiseBoard();
+	}
+
+    void InitialiseBoard()
+    {
+        currentBoard = BoardGenerator.GenerateBoard();
+
+        for (int x = 0; x < currentBoard.tiles.GetLength(0); x++)
+        {
+            string line = "";
+            for (int y = 0; y < currentBoard.tiles.GetLength(1); y++)
+            {
+                line += (x + y) % 2 == 0 ? "[#]" : "[_]";
+            }
+            Debug.Log(line);
+        }
+    }
+}
---
+++ Assets/Scripts/BoardGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class BoardGenerator {
+
+    public static Board GenerateBoard(int xSize = 8, int ySize = 8)
+    {
+        Tile[,] tiles = new Tile[xSize, ySize];
+
+        return new Board(tiles);
+    }
+}
+
+public class Board
+{
+    public Tile[,] tiles;
+
+    public Board(Tile[,] tiles)
+    {
+        this.tiles = tiles;
+    }
+}
+
+public enum Tile
+{
+    Plain,
+    Empty
+}
